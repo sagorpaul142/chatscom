4a6b7c1efe920c759913bb418c8e800d82d8ef77
sagorpaul142 <sagorpaulcse@gmail.com>
2024-02-14 21:13:57 +0600

    baseurl change

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
     // baseUrl = "http://localhost:8000/api";
     baseUrl = 'http://127.0.0.1:8000';
 } else if (process.env.NODE_ENV === 'production') {
-    baseUrl = 'https://seashell-app-yubac.ondigitalocean.app/api';
+    baseUrl = 'http://127.0.0.1:8000';
 } else {
     baseUrl = 'http://127.0.0.1:8000';
 }
